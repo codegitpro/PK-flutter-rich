6bee5b6c41b33b0c9dfefd7bc302048f09b1443b
codegitpro <michael.jin.dev@gmail.com>
2020-10-05 03:57:35 +0900

    use network image widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("I Am Rich"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-      ),
+          backgroundColor: Colors.deepPurpleAccent,
+          appBar: AppBar(
+            title: Text("I Am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage("https://i.ibb.co/4YN6x7R/original.jpg"),
+            ),
+          )),
     ),
   );
 }
