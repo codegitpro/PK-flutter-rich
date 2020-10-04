0d7f6bfa477e0b1bdbc461e4e60812686b0b9d41
codegitpro <michael.jin.dev@gmail.com>
2020-10-05 00:11:52 +0900

    format code with dartfmt

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello world'))));
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  );
 }
